2b9e2135c7c2fea5e13cd2751d0ca5597b1751d9
mackliu <macklun@gmail.com>
2024-11-22 14:28:06 +0800

    界面(interface)練習

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
 <body>
 <h1>物件的宣告</h1>
 <?php
+Interface Behavior{
+    public function run();
+    public function speed();
+    public function jump();
+}
 
 class Animal{
 protected $type='animal';
@@ -58,15 +63,21 @@ echo $cat->getName();
 <h1>繼承</h1>
 <?php
 
-class Cat extends Animal{
+class Cat extends Animal implements Behavior{
     protected $type='cat';
     protected $name="Judy";
     function __construct($hair_color){
         $this->hair_color=$hair_color;
     }
 
+    function jump(){
+        echo $this->name . " jumpping 2m";
+    }
 
 }
+
+
+
 
 
 $mycat=new Cat('white');
@@ -78,8 +89,10 @@ echo "<br>";
 echo $mycat->speed();
 echo "<br>";
 $mycat->setName("judy");
+
 echo $mycat->getName();
 echo "<br>";
+echo $mycat->jump();
 
 
 ?>
